84141981e8f2e7022f4506913e6636c303fc06ea
LassazVegaz <lasindunuwanga@gmail.com>
2022-06-22 17:39:33 +0530

    blogs funcs rest

--- backend/routers/blogs.router.js
+++ backend/routers/blogs.router.js
@@ -5,14 +5,53 @@ const _router = new KoaRouter({
 	prefix: "/blogs",
 });
 
-_router.get("/", (ctx) => {
-	ctx.body = {
-		name: "scene",
-	};
+_router.get("/", async (ctx) => {
+	try {
+		ctx.body = await blogsService.getBlogs();
+	} catch (error) {
+		ctx.body = error.message;
+		ctx.status = 500;
+	}
+});
+
+_router.get("/:id", async (ctx) => {
+	try {
+		ctx.body = await blogsService.getBlog(ctx.params.id);
+	} catch (error) {
+		ctx.body = error.message;
+		ctx.status = 500;
+	}
 });
 
 _router.post("/", async (ctx) => {
-	ctx.body = await blogsService.createBlog(ctx.request.body);
+	try {
+		ctx.body = await blogsService.createBlog(ctx.request.body);
+	} catch (error) {
+		ctx.body = error.message;
+		ctx.status = 500;
+	}
+});
+
+_router.put("/:id", async (ctx) => {
+	try {
+		ctx.body = await blogsService.updateBlog(
+			ctx.params.id,
+			ctx.request.body
+		);
+	} catch (error) {
+		ctx.body = error.message;
+		ctx.status = 500;
+	}
+});
+
+_router.delete("/:id", async (ctx) => {
+	try {
+		await blogsService.deleteBlog(ctx.params.id);
+		ctx.body = null;
+	} catch (error) {
+		ctx.body = error.message;
+		ctx.status = 500;
+	}
 });
 
 export const blogsRouter = _router;
--- backend/services/blogs.service.js
+++ backend/services/blogs.service.js
@@ -12,6 +12,32 @@ const createBlog = async (blog) => {
 	return _blog;
 };
 
+const getBlogs = async () => {
+	return await Blog.find();
+};
+
+const getBlog = async (id) => {
+	return await Blog.findById(id);
+};
+
+const updateBlog = async (id, blog) => {
+	await Blog.findByIdAndUpdate(id, {
+		author: blog.author,
+		title: blog.title,
+		content: blog.content,
+	});
+
+	return getBlog(id);
+};
+
+const deleteBlog = async (id) => {
+	await Blog.findByIdAndDelete(id);
+};
+
 export const blogsService = {
 	createBlog,
+	getBlogs,
+	getBlog,
+	updateBlog,
+	deleteBlog,
 };
