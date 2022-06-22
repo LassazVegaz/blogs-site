35544ee06fde785ba7a4f6bf9a14a0d79492aa26
LassazVegaz <lasindunuwanga@gmail.com>
2022-06-22 17:24:03 +0530

    blogs model

---
+++ backend/models/blogs.model.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const schema = mongoose.Schema({
+	author: {
+		type: String,
+		required: true,
+	},
+	title: {
+		type: String,
+		required: true,
+	},
+	content: {
+		type: String,
+		required: true,
+	},
+});
+
+export const Blog = mongoose.model("blogs", schema);
